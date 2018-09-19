f9a597de6e3f6b52548450cfff5af3ddc723837d
mijugit <michal@amjsystem.pl>
2018-09-19 14:18:45 +0200

    Jest OK

--- IoT_Class.py
+++ IoT_Class.py
@@ -1,2 +1,10 @@
 class IoT_class:
-    Device_Name = ''
+    Device_Name = ''        # Nazwa uniwersalna urządzenia np. termometr
+    Device_Adress = ''      # Unikalny adres portfela urządzenia
+    Device_Type = ''        # Typ techniczny np. MC_DS18b20
+    Device_Status = ''      # Czy urządzenie jest zarejestrowane
+    Device_GPIO_Digital_count = ''      # Liczba pinów GPIO - Digital
+    Device_GPIO_Analog_count = ''       # Liczba pinów GPIO - Analog
+
+
+
