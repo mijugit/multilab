a27e671165fb4dbd79fc6be613e38e53110d3dc0
mijugit <michal@amjsystem.pl>
2018-09-19 15:40:24 +0200

    Zmiany Testowe

--- IoT_Class.py
+++ IoT_Class.py
@@ -1,12 +1,39 @@
 class IoT_Device:
-    Device_Name = ''        # Nazwa uniwersalna urządzenia np. termometr
-    Device_Adress = ''      # Unikalny adres portfela urządzenia
-    Device_Type = ''        # Typ techniczny np. MC_DS18b20
-    Device_Status = ''      # Czy urządzenie jest zarejestrowane
+    Device_Name = ''                    # Nazwa przyjazna dla urządzenia np. "termometr"
+    Device_Description = ''             # Opis urządzenia
+    Device_Wallet_Adress = ''           # Unikalny adres portfela urządzenia
+    Device_Wallet_Public_Key = ''       # Klucz publiczny portfela
+    Device_Wallet_Private_Key = ''      # Klucz prywatny portfela
+    Device_Wallet_Amount = ''           # Ostatnia znana ilość tokenów w portfelu
+    Device_ID = ''                      # ID Urządzenia
+    Device_IP = ''                      # Ostatnie znane IP urządzenia
+    Device_Software_Version = ''        # Numer wersji oprogramowania
+    Device_Owner_Adress = ''            # Unikalny adres użytkownika
+    Device_Type = ''                    # Typ techniczny np. MC_DS18b20
+    Device_Status = ''                  # Czy urządzenie jest zarejestrowane stan [0 - nie 1 - tak]
     Device_GPIO_Digital_count = ''      # Liczba pinów GPIO - Digital
     Device_GPIO_Analog_count = ''       # Liczba pinów GPIO - Analog
-    Test = ''
-    Test2 = ''
+    Device_ON_Off = ''                  # Czy urządzenie jest Online czy Offline [0 - Offline 1 - Online]
+    Device_Registration_Date = ''       # Data i czas rejestracji urządzenia
+    Device_Last_Activity_Date = ''      # Data i czas ostatniej aktywności
+    Device_Returned_Data_List = ''      # Lista danych lub stanów zwracanych przez urządzenie
+    Device_Returned_Data_Interval = ''  # Interwał zwracania danych przez urządzenie
+
+class IoT_User:
+    User_Name = ''                      # Nazwa użytkownika
+    User_Description = ''               # Opis użytkownika
+    User_Wallet_Adress = ''             # Unikalny adres portfela użytkownika
+    User_Wallet_Public_Key = ''         # Klucz publiczny portfela użytkownika
+    User_Wallet_Private_Key = ''        # Klucz prywatny portfela użytkownika
+    User_Wallet_Amount = ''             # Oststnia znana ilość tokenów w portfelu
+    User_ID = ''                        # Unikalny ID użytkownika
+    User_Email_Adress = ''              # email adres użytkownika
+    User_Password   = ''                # hasło użytkownika
+    User_MFA_Status = ''                # Status MFA użytkownika
+    User_IP = ''                        # Oststnie znane IP użytkownika
+    User_Last_Activity_Date =''         # Data i czas oststniej aktywności użytkownika
+
+
 
 
 
